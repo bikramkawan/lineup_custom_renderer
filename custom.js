66f4d4a4b3ab25afed4557df3413d1613eec57b5
bikramkawan <bikramkawan@gmail.com>
2016-10-21 13:58:24 +0200

    custom heatmap
    Dummy custom heatmap

--- custom.js
+++ custom.js
@@ -1,71 +1,168 @@
-
 var categoricalCellRenderer = LineUpJS.renderer.createRenderer('g.my', function ($col, col, rows, context) {
-      var $rows = $col.datum(col).selectAll('g.my').data(rows, context.rowKey);
-      var $rows_enter = $rows.enter().append('g').attr({
-          'class': 'my',
-          'data-index': function (d, i) { return i; },
-          transform: function (d, i) { return 'translate(' + context.cellX(i) + ',' + context.cellPrevY(i) + ')'; }
-      });
-      $rows_enter.append('text').attr({
-          'clip-path': 'url(#' + context.idPrefix + 'clipCol' + col.id + ')',
-          x: function (d, i) { return context.rowHeight(i); }
-      });
-      $rows_enter.append('path');
-      $rows.attr({
-          'data-index': function (d, i) { return i; },
-          transform: function (d, i) { return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')'; }
-      });
-      $rows.select('text').attr({
-          x: function (d, i) { return context.rowHeight(i); }
-      }).text(function (d) { return col.getLabel(d); });
-      $rows.select('path').style({
-          fill: function (d) { return 'green'; }
-        }).attr({
-          d: function(d, i) {
+    var $rows = $col.datum(col).selectAll('g.my').data(rows, context.rowKey);
+    var $rows_enter = $rows.enter().append('g').attr({
+        'class': 'my',
+        'data-index': function (d, i) {
+            return i;
+        },
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellPrevY(i) + ')';
+        }
+    });
+    $rows_enter.append('text').attr({
+        'clip-path': 'url(#' + context.idPrefix + 'clipCol' + col.id + ')',
+        x: function (d, i) {
+            return context.rowHeight(i);
+        }
+    });
+    $rows_enter.append('path');
+    $rows.attr({
+        'data-index': function (d, i) {
+            return i;
+        },
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')';
+        }
+    });
+    $rows.select('text').attr({
+        x: function (d, i) {
+            return context.rowHeight(i);
+        }
+    }).text(function (d) {
+        rows.forEach(function (i) {
+
+
+        })
+
+        return col.getLabel(d);
+    });
+    $rows.select('path').style({
+        fill: function (d) {
+            return 'green';
+        }
+    }).attr({
+        d: function (d, i) {
             var size = Math.max(context.rowHeight(i) - context.option('rowPadding', 1) * 2, 0);
             var index = col.categories.indexOf(col.getValue(d));
             var s = d3.svg.symbol().type(d3.svg.symbolTypes[index]).size(100);
             return s();
-          },
-          transform: function(d, i) {
-            var size = Math.max(context.rowHeight(i) - context.option('rowPadding', 1) * 2, 0)/2;
-            return 'translate('+size+','+size+')';
-          }
-      });
-      context.animated($rows).attr({
-          transform: function (d, i) { return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')'; }
-      });
-      $rows.exit().remove();
-  });
+        },
+        transform: function (d, i) {
+            var size = Math.max(context.rowHeight(i) - context.option('rowPadding', 1) * 2, 0) / 2;
+            return 'translate(' + size + ',' + size + ')';
+        }
+    });
+    context.animated($rows).attr({
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')';
+        }
+    });
+    $rows.exit().remove();
+});
 
 //expecting in the column an object with { mean, min, max} in the range 0...100
 var MyColumn = LineUpJS.model.defineColumn('Custom', {
-  compare: function (a, b) {
-      return this.getValue(a).mean - this.getValue(b).mean;
-  }
-  });
+    compare: function (x, y) {
+
+        return this.getValue(x).mean - this.getValue(y).mean;
+    }
+});
+
+
+var MyColumn2 = LineUpJS.model.defineColumn('Custom', {
+    compare: function (a, b) {
+        return this.getValue(a).mean - this.getValue(b).mean;
+    }
+});
+
+// console.log(MyColumn1);
 
 var myCellRenderer = LineUpJS.renderer.createRenderer('path.shift', function ($col, col, rows, context) {
-      var $rows = $col.datum(col).selectAll('path.shift').data(rows, context.rowKey);
-      var $rows_enter = $rows.enter().append('path').attr({
-          'class': 'shift',
-          'data-index': function (d, i) { return i; },
-          transform: function (d, i) { return 'translate(' + context.cellX(i) + ',' + context.cellPrevY(i) + ')'; }
-      });
-      var f = col.getWidth() / 100;
-      $rows.attr('d', function(d, i) {
+    var $rows = $col.datum(col).selectAll('path.shift').data(rows, context.rowKey);
+    var $rows_enter = $rows.enter().append('path').attr({
+        'class': 'shift',
+        'data-index': function (d, i) {
+            return i;
+        },
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellPrevY(i) + ')';
+        }
+    });
+    var f = col.getWidth() / 100;
+    $rows.attr('d', function (d, i) {
         var value = col.getValue(d);
+
         var left = value.min * f, right = value.max * f, center = value.mean * f;
         var top = context.option('rowPadding', 1);
         var bottom = Math.max(context.rowHeight(i) - top, 0);
-        var middle = (bottom-top) / 2;
-        return 'M'+left+','+middle+'L'+right+','+middle +
-          'M'+left+','+top+'L'+left+','+bottom+
-          'M'+center+','+top+'L'+center+','+bottom+
-          'M'+right+','+top+'L'+right+','+bottom;
-      })
-      context.animated($rows).attr({
-          transform: function (d, i) { return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')'; }
-      });
-      $rows.exit().remove();
-  });
+        var middle = (bottom - top) / 2;
+        return 'M' + left + ',' + middle + 'L' + right + ',' + middle +
+            'M' + left + ',' + top + 'L' + left + ',' + bottom +
+            'M' + center + ',' + top + 'L' + center + ',' + bottom +
+            'M' + right + ',' + top + 'L' + right + ',' + bottom;
+    })
+    context.animated($rows).attr({
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')';
+        }
+    });
+    $rows.exit().remove();
+});
+
+
+var myCellRenderer2 = LineUpJS.renderer.createRenderer('g.my', function ($col, col, rows, context) {
+    var colors = ['#27AE60', '#f4ee42', '#42f48f', '#f45942', '#2980B9'];
+    var height = 5;
+    var width = [];
+    var y = 0;
+    var scale = d3.scale.linear().range([0, 100]); // Constraint the window width
+    var max = 0;
+
+    rows.forEach(function (d, i) {
+        max = d3.max([max, d3.sum(d.custom2.x)]);
+        scale.domain([0, max]);  // Convert any range to fix with in window width
+    });
+
+    var $rows = $col.datum(col).selectAll('g.my').data(rows, context.rowKey);
+
+    var $rows_enter = $rows.enter().append('g').attr({
+        'class': 'my',
+        'data-index': function (d, i) {
+            return i;
+        },
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellPrevY(i) + ')';
+        }
+    });
+
+    var $rects = $rows_enter.selectAll('rect').data(function (d) {
+        return d.custom2.x
+    }); // Select custom2.x data
+    $rects.enter().append('rect');
+    $rects.attr({
+        'data-index': function (d, i) {
+            return i;
+        },
+
+        'width': function (d) {
+            return scale(d);
+        },
+        'height': height,
+        'y': y,
+        'x': function (d) {
+            var prev = this.previousSibling; // One previous step information.
+            return (prev === null) ? 0 : parseFloat(d3.select(prev).attr('x')) + parseFloat(d3.select(prev).attr('width'));
+        },
+        'fill': function (d, i) {
+            return colors[i];
+        }
+    });
+
+    context.animated($rows).attr({
+        transform: function (d, i) {
+            return 'translate(' + context.cellX(i) + ',' + context.cellY(i) + ')';
+        }
+    });
+    $rows.exit().remove();
+});
+
--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 /**
  * Created by Samuel Gratzl on 04.09.2014.
  */
-
+// Self Invoking JS
 (function (LineUpJS, d3) {
   var arr = [
-      {a: 10, b: 20, c: 30, d: 'Row1', cat: 'c1', custom: { min: 10, max: 100, mean: 30}},
-      {a: 5, b: 14, c: 2, d: 'Row2', cat: 'c2', custom: { min: 10, max: 100, mean: 80}},
-      {a: 2, b: 7, c: 100, d: 'Row3', cat: 'c3', custom: { min: 10, max: 80, mean: 30}},
-      {a: 7, b: 1, c: 60, d: 'Row4dasfa dsfasdf', cat: 'c1', custom: { min: 20, max: 70, mean: 50}}
-    ];
+      {a: 10, b: 20, c: 30, d: 'Row1', cat: 'c1', custom: { min: 10, max: 100, mean: 30}, custom2: { x:[10,22,44,66,88]}},
+      {a: 5, b: 14, c: 2, d: 'Row2', cat: 'c2', custom: { min: 10, max: 100, mean: 80},custom2: { x:[10,22,44,66,88]}},
+        {a: 2, b: 7, c: 100, d: 'Row3', cat: 'c3', custom: { min: 10, max: 80, mean: 30},custom2: { x:[10,22,44,66,88]}},
+      {a: 7, b: 1, c: 60, d: 'Row4', cat: 'c1', custom: { min: 20, max: 70, mean: 50},custom2: { x:[10,22,44,66,88]}},
+      {a: 7, b: 1, c: 60, d: 'Row4', cat: 'c1', custom: { min: 60, max: 90, mean: 80},custom2: { x:[10,22,44,66,88]}},
+   ];
+
     var desc = [
       {label: 'D', type: 'string', column: 'd'},
       {label: 'C', type: 'number', column: 'c', 'domain': [0, 120], color: 'green'},
+     {label: 'A', type: 'number', column: 'a', 'domain': [0, 120], color: 'green'},
       {label: 'Cat', type: 'categorical', column: 'cat', categories : ['c1','c2','c3']},
-      {label: 'Custom', type: 'custom', column: 'custom'}
+      {label: 'Custom1', type: 'custom', column: 'custom'},
+      {label: 'Custom2', type: 'custom2', column: 'custom2'}
+      //  {label: 'Custom3', type: 'custom', column: 'custom3'}
     ];
 
     var p = new LineUpJS.provider.LocalDataProvider(arr, desc, {
       columnTypes: {
-        custom: MyColumn
+        custom: MyColumn,
+        custom2: MyColumn2
+
+
       }
     });
     var r = p.pushRanking();
 
     var root = d3.select('main');
-    r.push(p.create(desc[0]));
-    r.push(p.create(desc[3]));
-    r.push(p.create(desc[1]));
-    r.push(p.create(desc[2]));
+     desc.forEach(function (d) {
+
+
+        r.push(p.create(d));
+    })
 
     var body = LineUpJS.create(p, root.node(), {
       renderers: {
         custom: myCellRenderer,
-        categorical: categoricalCellRenderer
+        custom2: myCellRenderer2,
+          categorical: categoricalCellRenderer
+
       }
     });
     body.update();
